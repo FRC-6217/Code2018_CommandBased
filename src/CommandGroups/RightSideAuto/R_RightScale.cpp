dec54338d64acdf5bb26da1a04c4e89e6bb9029f
josephtcoyle <jtcoyle@startech-comp.com>
2018-02-16 20:52:54 -0600

    Added factors of height to command groups

--- src/CommandGroups/RightSideAuto/R_RightScale.cpp
+++ src/CommandGroups/RightSideAuto/R_RightScale.cpp
@@ -3,7 +3,7 @@
 R_RightScale::R_RightScale() {
 	AddSequential(new DriveDistance(LR_SCALE_FIRST_DISTANCE));
 	AddSequential(new TurnDegrees(R_TURN_LEFT_DEGREES));
-	AddParallel(new AutoLift());
+	AddParallel(new AutoLift(LIFTING_TO_SCALE,0));
 	AddSequential(new DriveDistance(LR_SCALE_SECOND_DISTANCE));
 	AddSequential(new spitOutCube());
 }
