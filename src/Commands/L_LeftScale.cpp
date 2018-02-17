53196ecd44b693e8dc0292f29c74e174b47cae64
josephtcoyle <jtcoyle@startech-comp.com>
2018-02-16 18:33:44 -0600

    Finish L_LeftScale

--- src/Commands/L_LeftScale.cpp
+++ src/Commands/L_LeftScale.cpp
@@ -3,7 +3,9 @@
 L_LeftScale::L_LeftScale() {
 	AddSequential(new DriveDistance(LR_SCALE_FIRST_DISTANCE));
 	AddSequential(new TurnDegrees(L_SCALE_DEGREES));
+	//AddParallel(new AutoLift());
+	//once added a command called AutoLift uncomment the line above this one
 	AddSequential(new DriveDistance(LR_SCALE_SECOND_DISTANCE));
-
+	AddSequential(new spitOutCube());
 
 }
